73e58d0d76dbebd3bd905802bf01fb2741c9dc92
nicolasPerezLeal <nico_95_rio2@hotmail.com>
2020-08-10 20:03:28 -0300

    original

--- MiPrimerProyecto/Program.cs
+++ MiPrimerProyecto/Program.cs
@@ -9,11 +9,7 @@ namespace MiPrimerProyecto
     static class Program
     {
         /// <summary>
-<<<<<<< HEAD
-        /// Punto de entrada principal para la aplicación en C#.
-=======
-        /// Punto de entrada principal para la aplicación Java y c+.
->>>>>>> d6c738b3d7f8b8e017da63faed889f0300c5a280
+        /// Punto de entrada principal para la aplicación.
         /// </summary>
         [STAThread]
         static void Main()
