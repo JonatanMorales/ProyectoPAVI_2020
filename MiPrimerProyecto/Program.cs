6d28013baa12b438b2a0cd7ffa3535c44a6e1e5d
corbellamariana <corbellamariana@gmail.com>
2020-08-10 19:43:21 -0300

    Generación de conflicto Mariana

--- MiPrimerProyecto/Program.cs
+++ MiPrimerProyecto/Program.cs
@@ -9,7 +9,7 @@ namespace MiPrimerProyecto
     static class Program
     {
         /// <summary>
-        /// Punto de entrada principal para la aplicación.
+        /// Punto de entrada principal para la aplicación en C#.
         /// </summary>
         [STAThread]
         static void Main()
