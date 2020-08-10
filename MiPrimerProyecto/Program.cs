781f3179ea80fa2cd568dc04e1828c1cdb2c5a64
JonatanMorales <moralesjonatan66257@gmail.com>
2020-08-10 20:21:11 -0300

    comentario PAV1

--- MiPrimerProyecto/Program.cs
+++ MiPrimerProyecto/Program.cs
@@ -9,7 +9,7 @@ namespace MiPrimerProyecto
     static class Program
     {
         /// <summary>
-        /// Punto de entrada principal para la aplicación.
+        /// Punto de entrada principal para la aplicación PAV1.
         /// </summary>
         [STAThread]
         static void Main()
